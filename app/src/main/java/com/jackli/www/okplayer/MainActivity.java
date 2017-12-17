f4f2df26e720b1217ea178399ee907bcd252f9f6
lijiwei <lijiwei@wsdashi.com>
2017-12-17 11:52:54 +0800

    创建OkPlayer项目

---
+++ app/src/main/java/com/jackli/www/okplayer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jackli.www.okplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
